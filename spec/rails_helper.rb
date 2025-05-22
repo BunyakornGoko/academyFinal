f73c9fab98cf2d4a3bda2d6edcc61ac0032f0566
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2025-05-22 13:14:21 +0700

    Comment headless for test on browser

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -83,7 +83,7 @@ Capybara.register_driver :selenium_chrome_headless do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   
   # Chrome configurations
-  options.add_argument('--headless')
+  # options.add_argument('--headless')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--disable-gpu')
